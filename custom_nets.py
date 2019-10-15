4e9623c6ab3ba3d79b2ddbc9f9e3825b32b6fd4e
Ben Albert <benalbert667@gmail.com>
2019-10-15 05:29:39 -0700

    need gpu to train

--- custom_nets.py
+++ custom_nets.py
@@ -2,6 +2,31 @@ import torch
 import torch.nn as nn
 
 
+class LayeredLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers=1):
+        super(LayeredLSTM, self).__init__()
+
+        self.i_s = input_size
+        self.h_s = hidden_size
+        self.n_layers = num_layers
+
+        self.hidden_states = [None for _ in range(self.n_layers)]
+        self.cell_states = [None for _ in range(self.n_layers)]
+        self.lstms = nn.ModuleList([LSTM1(self.i_s, self.h_s) for _ in range(self.n_layers)])
+
+    def init_new_seq(self, random=False):
+        for i in range(self.n_layers):
+            self.hidden_states[i], self.cell_states[i] = (_ for _ in self.lstms[i].init_states(random=random))
+
+    def forward(self, x):
+        h = x
+        for i in range(self.n_layers):
+            lstm, hidden, cell = self.lstms[i], self.hidden_states[i], self.cell_states[i]
+            y, h, c = lstm(h, hidden.view(1, -1), cell)
+            self.hidden_states[i], self.cell_states[i] = h, c
+        return y
+
+
 class LSTM1(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(LSTM1, self).__init__()
@@ -14,12 +39,59 @@ class LSTM1(nn.Module):
         self.o_gate = nn.Sequential(nn.Linear(self.i_s + self.h_s, self.h_s, bias=True), nn.Sigmoid())
         self.c_gate = nn.Sequential(nn.Linear(self.i_s + self.h_s, self.h_s, bias=True), nn.Tanh())
         self.tanh = nn.Tanh()
+        self.softmax = nn.LogSoftmax(dim=1)
 
-    def init_states(self):
+    def init_states(self, random=False):
+        if random:
+            return torch.rand(self.h_s), torch.rand(self.h_s)
         return torch.zeros(self.h_s), torch.zeros(self.h_s)
 
     def forward(self, x, h, cs):  # input, hidden state, cell state
         x = torch.cat((x, h), 1)
         f, i, o, c = self.f_gate(x), self.i_gate(x), self.o_gate(x), self.c_gate(x)
         cs = (cs * f) + (i * c)
-        return o * self.tanh(cs), cs
+        h = o * self.tanh(cs)
+        return self.softmax(h), h, cs
+
+
+class LSTM2(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(LSTM2, self).__init__()
+
+        self.i_s = input_size
+        self.h_s = hidden_size
+
+        self.f_gate = nn.Sequential(
+            nn.Linear(self.i_s + self.h_s, self.i_s*2//3 + self.h_s, bias=True),
+            nn.Linear(self.i_s*2//3 + self.h_s, self.i_s//3 + self.h_s, bias=True),
+            nn.Linear(self.i_s//3 + self.h_s, self.h_s, bias=True),
+            nn.Sigmoid())
+        self.i_gate = nn.Sequential(
+            nn.Linear(self.i_s + self.h_s, self.i_s*2//3 + self.h_s, bias=True),
+            nn.Linear(self.i_s*2//3 + self.h_s, self.i_s//3 + self.h_s, bias=True),
+            nn.Linear(self.i_s//3 + self.h_s, self.h_s, bias=True),
+            nn.Sigmoid())
+        self.o_gate = nn.Sequential(
+            nn.Linear(self.i_s + self.h_s, self.i_s*2//3 + self.h_s, bias=True),
+            nn.Linear(self.i_s*2//3 + self.h_s, self.i_s//3 + self.h_s, bias=True),
+            nn.Linear(self.i_s//3 + self.h_s, self.h_s, bias=True),
+            nn.Sigmoid())
+        self.c_gate = nn.Sequential(
+            nn.Linear(self.i_s + self.h_s, self.i_s*2//3 + self.h_s, bias=True),
+            nn.Linear(self.i_s*2//3 + self.h_s, self.i_s//3 + self.h_s, bias=True),
+            nn.Linear(self.i_s//3 + self.h_s, self.h_s, bias=True),
+            nn.Tanh())
+        self.tanh = nn.Tanh()
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def init_states(self, random=False):
+        if random:
+            return torch.rand(self.h_s), torch.rand(self.h_s)
+        return torch.zeros(self.h_s), torch.zeros(self.h_s)
+
+    def forward(self, x, h, cs):  # input, hidden state, cell state
+        x = torch.cat((x, h), 1)
+        f, i, o, c = self.f_gate(x), self.i_gate(x), self.o_gate(x), self.c_gate(x)
+        cs = (cs * f) + (i * c)
+        h = o * self.tanh(cs)
+        return self.softmax(h), h, cs
---
+++ run_on_gpu.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from custom_nets import LayeredLSTM
+from random import shuffle
+# %%
+
+with open('trump_tweets.txt', 'r') as file:
+    data = file.readlines()
+# categories = set(_ for d in data for _ in d)
+categories = ('\n', ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '+', ',', '-', '.', '/',
+              '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '=', '?', '@',
+              'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+              'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', 'a', 'b', 'c', 'd', 'e',
+              'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+              'v', 'w', 'x', 'y', 'z', '<END>')
+for i in range(len(data)):
+    data[i] = data[i].strip()
+    chrs = set(data[i])
+    for erase in chrs - set(categories):
+        data[i] = data[i].replace(erase, '')
+
+n_letters = len(categories)
+
+
+gpu = torch.device('cuda:0')
+
+
+def input_target_pair(s):
+    if s in categories:  # inputting letter by letter
+        inp = torch.zeros(1, 1, n_letters, device=gpu)
+        inp[0, 0, categories.index(s)] = 1
+        return inp
+    inp = torch.zeros(len(s), 1, n_letters, device=gpu)
+    tar = []
+    for i, c in enumerate(s):
+        if i > 0:
+            tar.append(categories.index(c))
+        inp[i, 0, categories.index(c)] = 1
+    tar.append(len(categories) - 1)
+    tar = torch.LongTensor(tar, device=gpu)
+    tar.unsqueeze_(-1)
+    return inp, tar
+
+
+for n_layers in (3, 6, 9, 12):
+    print('Training lstm of {} layers...'.format(n_layers))
+    net = LayeredLSTM(n_letters, n_letters, num_layers=n_layers).to(gpu)
+    criterion = nn.NLLLoss()
+    lr = 0.000005
+    optimizer = optim.Adam(net.parameters(), lr=lr)
+
+    for epoch in range(5):
+        count = 0
+        shuffle(data)
+        for d in data:
+            seq_len = len(d)
+            input_tensor, target_tensor = input_target_pair(d)
+            net.init_new_seq()
+
+            optimizer.zero_grad()
+
+            loss = None
+
+            for i in range(seq_len):
+                output = net(input_tensor[i])
+                l = criterion(output, target_tensor[i])
+                if loss is None:
+                    loss = l
+                else:
+                    loss += l
+
+            if loss is None:
+                continue
+
+            loss.backward()
+
+            optimizer.step()
+
+            count += 1
+            if not count % 30:
+                print('|', end='')
+
+        print('\nepoch {} done'.format(epoch))
+    print()
+
+    torch.save(net, 'nets/trump{}.pt'.format(n_layers))
+
